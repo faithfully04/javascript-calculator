6363f4fe7dadb550f173ad3ca7c50a7b9f12d256
faithfully04 <faithfully04@yahoo.com>
2022-06-27 02:09:49 +0100

    improve the javascript calculator

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-alert
+let question = prompt ("Which operation do you want to perform? e.g +, -, / or *")
+
+let num1 = parseInt(prompt("First number: "));
+let num2 = parseInt(prompt("Second number: "));
+
+if (question == "+") {
+    console.log(num1 + num2)
+} else if (question == "-") {
+    console.log(num1 - num2)
+} else if (question == "/") {
+    console.log(num1 / num2);
+} else if (question == "*") {
+    console.log(num1 * num2);
+}
+
